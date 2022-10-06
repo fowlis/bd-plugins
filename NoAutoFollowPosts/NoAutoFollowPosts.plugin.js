25ce2ecb32ee5ef050510271342266fcc005c198
fowlis <fowlis@protonmail.com>
2022-10-06 14:40:53 +0100

    upload blank project
    will work on this after reminders

---
+++ NoAutoFollowPosts/NoAutoFollowPosts.plugin.js
@@ -0,0 +1,13 @@
+/**
+ * @name NoAutoFollowPosts
+ * @author slughead
+ * @description Stops Discord from auto following posts when you type in them.
+ * @version 0.0.1
+ */
+
+ module.exports = class NAFPplugin {
+    start() {
+    }
+    stop() {
+    }
+}
